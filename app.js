9951cd6911a53c974385cdbe16b4b2dcd538be74
Joan-Wangare <80474390+Joan-Wangare@users.noreply.github.com>
2022-03-15 22:11:17 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,47 +1,63 @@
 // SPLASHSCREEN
 
-const logo = document.querySelectorAll('#logo path');
+const logo = document.querySelectorAll("#logo path");
 
-for(let i = 0; i<logo.length; i++){
-    console.log('Letter ' +i + ' is ' +logo[i].getTotalLength());
+for (let i = 0; i < logo.length; i++) {
+  //   console.log("Letter " + i + " is " + logo[i].getTotalLength());
+  console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
 }
 
 // SIGN UP
 
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay');
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
 
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
+// CART SIDEBAR CODE
+const addToCart = document.querySelectorAll(".add-to-cart-btn");
+const cartPopSidebar = document.querySelector(".cart-sidebar");
+addToCart.forEach(function (btn) {
+  btn.addEventListener("click", () => {
+    cartPopSidebar.classList.add("showCart");
+    setTimeout(hideCart, 4000); //CALL THE hideCart AFTER 3secs of display
+  });
+});
 
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.form')
-        closeModal(modal)
-    })
-})
+function hideCart() {
+  //FUNCTION TO HIDE THE CART AFTER DISPLAY
+  cartPopSidebar.classList.remove("showCart");
+}
+// END OF CART SIDEBAR CODE
 
-overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.form.active')
-    modals.forEach(modal => {
-        closeModal(modal)
-    })
-})
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".form");
+    closeModal(modal);
+  });
+});
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".form.active");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
 
 function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
 }
 
 function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
 }
-
