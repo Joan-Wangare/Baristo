ec99f8d5453ecf8e622e951bc880e14a8d61b4a5
Joan-Wangare <80474390+Joan-Wangare@users.noreply.github.com>
2022-03-15 13:17:33 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+// SPLASHSCREEN
+
+const logo = document.querySelectorAll('#logo path');
+
+for(let i = 0; i<logo.length; i++){
+    console.log('Letter ' +i + ' is ' +logo[i].getTotalLength());
+}
+
+// SIGN UP
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.form')
+        closeModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.form.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
