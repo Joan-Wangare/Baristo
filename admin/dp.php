e0981cdf2f37df71904f11f91d88e94bf0c37a5d
Joan <joanwangare01@gmail.com>
2022-03-25 01:19:15 +0300

    new changes

---
+++ admin/dp.php
@@ -0,0 +1,17 @@
+<?php
+
+//declaring MuSQL variables
+
+    $db_host = "localhost";
+    $db_user ="root";
+    $db_pwd ="";
+    $db_name ="CMS";
+
+//Connect query
+
+    $conn = new mysqli($db_host, $db_user, $db_pwd, $db_name) or die ("FAILED TO CONNECT");
+
+    // if($conn){
+    //     echo "We are connected";
+    // }
+?> 
